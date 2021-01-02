327fd4824c5138b457050b9b5cfd1c369e15f979
moritz <fink.moritz@gmail.com>
2021-01-03 00:59:34 +0100

    Display other player's name on join

---
+++ csbcgf-unity/Assets/Scripts/Game/CameraWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float cameraPositionZ = -10f;
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            cameraPositionZ = 10f;
+        }
+        Camera.main.transform.position = new Vector3(0f, 1f, cameraPositionZ);
+        Camera.main.transform.LookAt(new Vector3());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
