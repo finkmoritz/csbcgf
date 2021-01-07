9113db5acb3471e25a589923edf7a5951ddca6aa
moritz <fink.moritz@gmail.com>
2021-01-07 19:45:18 +0100

    Add missing files to VCS

---
+++ csbcgf/Library/Event/Implementations/CardEvent/PlayCard/PlaySpellCardEvent/PlayTargetlessSpellCardEvent/EndPlayTargetlessSpellCardEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csbcgf
+{
+    [Serializable]
+    public class EndPlayTargetlessSpellCardEvent : EndPlaySpellCardEvent
+    {
+        public EndPlayTargetlessSpellCardEvent(ITargetlessSpellCard spellCard)
+            : base(spellCard)
+        {
+        }
+
+        public EndPlayTargetlessSpellCardEvent(Func<ITargetlessSpellCard> getSpellCard)
+            : base(getSpellCard)
+        {
+        }
+    }
+}
