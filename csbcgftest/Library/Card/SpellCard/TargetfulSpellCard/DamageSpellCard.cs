283a5584bc126bf63be7893e292fd85449c9f96a
moritz <fink.moritz@gmail.com>
2021-09-12 10:32:13 +0200

    Migrate csbcgftest to .net5

---
+++ csbcgftest/Library/Card/SpellCard/TargetfulSpellCard/DamageSpellCard.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using csbcgf;
+using Newtonsoft.Json;
+
+namespace csbcgftest
+{
+    [Serializable]
+    public class DamageSpellCard : TargetfulSpellCard
+    {
+        [JsonProperty]
+        protected uint damage;
+
+        public DamageSpellCard(uint damage)
+            : this(damage, new List<ICardComponent>(), new List<IReaction>())
+        {
+            Components.Add(new DamageSpellCardComponent((int)damage, damage));
+        }
+
+        [JsonConstructor]
+        protected DamageSpellCard(
+            uint damage,
+            List<ICardComponent> components,
+            List<IReaction> reactions
+            ) : base(components, reactions)
+        {
+            this.damage = damage;
+        }
+
+        public override object Clone()
+        {
+            List<ICardComponent> componentsClone = new List<ICardComponent>();
+            Components.ForEach(c => componentsClone.Add((ICardComponent)c.Clone()));
+
+            List<IReaction> reactionsClone = new List<IReaction>();
+            Reactions.ForEach(r => reactionsClone.Add((IReaction)r.Clone()));
+
+            return new DamageSpellCard(
+                damage,
+                componentsClone,
+                reactionsClone
+            );
+        }
+
+        [Serializable]
+        public class DamageSpellCardComponent : TargetfulSpellCardComponent
+        {
+            [JsonProperty]
+            private readonly uint damage;
+
+            public DamageSpellCardComponent(int mana, uint damage)
+                : this(damage, new ManaCostStat(mana, mana), new List<IReaction>())
+            {
+            }
+
+            [JsonConstructor]
+            public DamageSpellCardComponent(
+                uint damage,
+                ManaCostStat manaCostStat,
+                List<IReaction> reactions
+                ) : base(manaCostStat, reactions)
+            {
+                this.damage = damage;
+            }
+
+            public override void Cast(IGame game, ICharacter target)
+            {
+                game.Execute(new ModifyLifeStatAction(target, -(int)damage));
+            }
+
+            public override HashSet<ICharacter> GetPotentialTargets(IGameState gameState)
+            {
+                HashSet<ICharacter> targets = new HashSet<ICharacter>();
+                foreach (IPlayer player in gameState.Players)
+                {
+                    targets.Add(player);
+                    player.Board.AllCards.ForEach(c => targets.Add((ICharacter)c));
+                }
+                return targets;
+            }
+
+            public override object Clone()
+            {
+                List<IReaction> reactionsClone = new List<IReaction>();
+                foreach (IReaction reaction in Reactions)
+                {
+                    reactionsClone.Add((IReaction)reaction.Clone());
+                }
+
+                return new DamageSpellCardComponent(
+                    damage,
+                    (ManaCostStat)manaCostStat.Clone(),
+                    reactionsClone
+                );
+            }
+        }
+    }
+}
---
+++ csbcgftest/Test.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using csbcgf;
+using NUnit.Framework;
+
+namespace csbcgftest
+{
+    [TestFixture()]
+    public class Test
+    {
+        private IGame game;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            List<IPlayer> players = new List<IPlayer>();
+            for (int i=0; i<2; ++i)
+            {
+                IDeck deck = new Deck();
+
+                for (int j=0; j<3; ++j)
+                {
+                    ICard fireball = new DamageSpellCard(3);
+                    deck.Push(fireball);
+                }
+
+                for (int j=0; j<2; ++j)
+                {
+                    ICard goblin = new MonsterCard(2, 1, 2);
+                    deck.Push(goblin);
+                }
+
+                players.Add(new Player(deck));
+            }
+
+            game = new Game(players);
+            game.StartGame(initialHandSize: 1, initialPlayerLife: 2);
+        }
+
+        [Test()]
+        public void TestInitialConditions()
+        {
+            Assert.AreEqual(1, game.ActivePlayer.ManaValue);
+            Assert.AreEqual(0, game.NonActivePlayers[0].ManaValue);
+            Assert.AreEqual(1, game.ActivePlayer.ManaBaseValue);
+            Assert.AreEqual(0, game.NonActivePlayers[0].ManaBaseValue);
+
+            Assert.AreEqual(3, game.ActivePlayer.Deck.Size);
+            Assert.AreEqual(4, game.NonActivePlayers[0].Deck.Size);
+            Assert.AreEqual(2, game.ActivePlayer.Hand.Size);
+            Assert.AreEqual(1, game.NonActivePlayers[0].Hand.Size);
+
+            IMonsterCard goblin = (IMonsterCard)game.ActivePlayer.Hand[0];
+            Assert.AreEqual(2, goblin.ManaValue);
+            Assert.AreEqual(1, goblin.AttackValue);
+            Assert.AreEqual(2, goblin.LifeValue);
+
+            Assert.False(goblin.IsSummonable(game));
+        }
+
+        [Test()]
+        public void TestGame()
+        {
+            IMonsterCard goblin = (IMonsterCard)game.ActivePlayer.Hand[0];
+            Assert.False(goblin.IsSummonable(game));
+
+            game.NextTurn(); //Second player's turn
+
+            Assert.AreEqual(1, game.ActivePlayer.ManaValue);
+            Assert.AreEqual(1, game.NonActivePlayers[0].ManaValue);
+            Assert.AreEqual(1, game.ActivePlayer.ManaBaseValue);
+            Assert.AreEqual(1, game.NonActivePlayers[0].ManaBaseValue);
+
+            Assert.AreEqual(3, game.ActivePlayer.Deck.Size);
+            Assert.AreEqual(3, game.NonActivePlayers[0].Deck.Size);
+            Assert.AreEqual(2, game.ActivePlayer.Hand.Size);
+            Assert.AreEqual(2, game.NonActivePlayers[0].Hand.Size);
+
+            game.NextTurn(); //First player's turn again
+
+            Assert.AreEqual(2, game.ActivePlayer.ManaValue);
+            Assert.AreEqual(1, game.NonActivePlayers[0].ManaValue);
+            Assert.AreEqual(2, game.ActivePlayer.ManaBaseValue);
+            Assert.AreEqual(1, game.NonActivePlayers[0].ManaBaseValue);
+
+            Assert.AreEqual(2, game.ActivePlayer.Deck.Size);
+            Assert.AreEqual(3, game.NonActivePlayers[0].Deck.Size);
+            Assert.AreEqual(3, game.ActivePlayer.Hand.Size);
+            Assert.AreEqual(2, game.NonActivePlayers[0].Hand.Size);
+
+            goblin = (IMonsterCard)game.ActivePlayer.Hand[0];
+            Assert.True(goblin.IsSummonable(game));
+
+            //Play monster card
+            Assert.True(game.ActivePlayer.Board.IsFreeSlot(0));
+            game.ActivePlayer.CastMonster(game, goblin, 0);
+            Assert.False(game.ActivePlayer.Board.IsFreeSlot(0));
+            Assert.AreEqual(1, game.ActivePlayer.Board.AllCards.Count);
+
+            Assert.AreEqual(0, game.ActivePlayer.ManaValue);
+            Assert.False(goblin.IsReadyToAttack);
+
+            game.NextTurn(); //Second player's turn again
+
+            Assert.AreEqual(2, game.ActivePlayer.ManaValue);
+            Assert.AreEqual(0, game.NonActivePlayers[0].ManaValue);
+            Assert.AreEqual(2, game.ActivePlayer.ManaBaseValue);
+            Assert.AreEqual(2, game.NonActivePlayers[0].ManaBaseValue);
+
+            IMonsterCard otherGoblin = (IMonsterCard)game.ActivePlayer.Hand[0];
+            game.ActivePlayer.CastMonster(game, otherGoblin, 0);
+
+            Assert.False(goblin.IsReadyToAttack);
+
+            game.NextTurn(); //First player's turn again
+
+            Assert.AreEqual(3, game.ActivePlayer.ManaValue);
+            Assert.AreEqual(0, game.NonActivePlayers[0].ManaValue);
+            Assert.AreEqual(3, game.ActivePlayer.ManaBaseValue);
+            Assert.AreEqual(2, game.NonActivePlayers[0].ManaBaseValue);
+
+            //Attack player
+            Assert.True(goblin.IsReadyToAttack);
+            Assert.AreEqual(2, game.NonActivePlayers[0].LifeValue);
+            goblin.Attack(game, game.NonActivePlayers[0]);
+            Assert.AreEqual(1, game.NonActivePlayers[0].LifeValue);
+            Assert.True(game.NonActivePlayers[0].IsAlive);
+            Assert.False(goblin.IsReadyToAttack);
+
+            game.NextTurn(); //Second player's turn again
+
+            Assert.AreEqual(2, goblin.LifeValue);
+            otherGoblin.Attack(game, goblin);
+            Assert.AreEqual(1, goblin.LifeValue);
+            Assert.AreEqual(1, otherGoblin.LifeValue);
+
+            game.NextTurn(); //First player's turn again
+
+            //Both monster cards die in battle
+            Assert.True(game.ActivePlayer.Graveyard.IsEmpty);
+            Assert.True(game.NonActivePlayers[0].Graveyard.IsEmpty);
+            Assert.False(game.ActivePlayer.Board.IsEmpty);
+            Assert.False(game.NonActivePlayers[0].Board.IsEmpty);
+            goblin.Attack(game, otherGoblin);
+            Assert.AreEqual(1, game.ActivePlayer.Graveyard.Size);
+            Assert.AreEqual(1, game.NonActivePlayers[0].Graveyard.Size);
+            Assert.True(game.ActivePlayer.Board.IsEmpty);
+            Assert.True(game.NonActivePlayers[0].Board.IsEmpty);
+
+            //Destroy second player
+            Assert.AreEqual(1, game.NonActivePlayers[0].LifeValue);
+            Assert.True(game.NonActivePlayers[0].IsAlive);
+
+            ITargetfulSpellCard fireball = (ITargetfulSpellCard)game.ActivePlayer.Hand[2];
+            game.ActivePlayer.CastSpell(game, fireball, game.NonActivePlayers[0]);
+
+            Assert.AreEqual(0, game.NonActivePlayers[0].LifeValue);
+            Assert.False(game.NonActivePlayers[0].IsAlive);
+        }
+
+        [Test()]
+        public void TestGameSerialization()
+        {
+            string serializedJson = CsbcgfJsonConvert.Serialize(game);
+            Game deserializedGame = CsbcgfJsonConvert.Deserialize<Game>(serializedJson);
+            string deserializedJson = CsbcgfJsonConvert.Serialize(deserializedGame);
+            Assert.AreEqual(serializedJson, deserializedJson);
+        }
+
+        [Test()]
+        public void TestGameCloning()
+        {
+            try
+            {
+                Game gameClone = (Game)game.Clone();
+                string serializedGame = CsbcgfJsonConvert.Serialize(game);
+                string serializedGameClone = CsbcgfJsonConvert.Serialize(gameClone);
+                Assert.AreEqual(serializedGame, serializedGameClone);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Game.Clone() failed!\n(Check for circular dependencies)\n" + e.ToString());
+            }
+        }
+    }
+}
